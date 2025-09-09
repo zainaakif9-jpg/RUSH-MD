62976e0c45fa2d6783f00bcf8db07429d931458a
zainaakif9-jpg <zainaakif9@gmail.com>
2025-09-09 16:03:45 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "VISPER-MD&GkAi2ZpS#Ha2jIplgAYRj12CWOnpWyTU6DF9uI04mSWWKzjndReQ",
+    SESSION_ID: process.env.SESSION_ID || "VISPER-MD&6hRnURqA#tI4Jr-ewWYK9206adcInwHY3gfsjcNLm_7NYwVMYckQ",
     // add your Session Id make sure it starts with VISPER-MD&
 
     CINESUB_API_KEY: process.env.CINESUB_API_KEY || "e0795fcd-58a9-4c0e-994d-648f146077cb",
